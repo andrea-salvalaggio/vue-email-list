a2bdb3bccf65c9fdd54c636d5cdece102977f80e
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-07-13 16:37:20 +0200

    Creo un ciclo for per visualizzare 10 email

--- js/script.js
+++ js/script.js
@@ -3,14 +3,21 @@ const app = new Vue (
         el: '#root',
 
         data: {
-            email: '',
+
+            emails: [],
+            
         },
 
-        created(){
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then((response) => {
-                this.email = response.data.response;
-            });
+        created() {
+
+            for(let i=0; i < 10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    this.emails.push(response.data.response);
+                    console.log(this.emails);
+                });
+            };           
+         
         },
     }
 );
